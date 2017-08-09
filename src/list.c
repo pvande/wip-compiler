1684426f6a139dc005543720d494774262e26e71
Pieter van de Bruggen <pvande@gmail.com>
2017-08-09 16:33:01 -0700

    Allowing for Lists that have zero-sized buckets (truly empty lists).

--- src/list.c
+++ src/list.c
@@ -11,16 +11,20 @@ void initialize_list(List* list, size_t bucket_count, size_t bucket_size) {
   list->capacity = bucket_size * bucket_count;
   list->bucket_size = bucket_size;
   list->length = 0;
-  list->buckets = malloc(bucket_count * sizeof(void*));
 
-  for (size_t i = 0; i < bucket_count; i++) {
-    list->buckets[i] = malloc(bucket_size * sizeof(void*));
+  if (bucket_size == 0) {
+    list->buckets = NULL;
+  } else {
+    list->buckets = malloc(bucket_count * sizeof(void*));
+
+    for (size_t i = 0; i < bucket_count; i++) {
+      list->buckets[i] = malloc(bucket_size * sizeof(void*));
+    }
   }
 }
 
 List* new_list(size_t bucket_count, size_t bucket_size) {
   assert(bucket_count > 0);
-  assert(bucket_size > 0);
 
   List* ret = malloc(sizeof(List));
   initialize_list(ret, bucket_count, bucket_size);
@@ -29,6 +33,11 @@ List* new_list(size_t bucket_count, size_t bucket_size) {
 }
 
 void* list_get(List* list, size_t idx) {
+  if (list->bucket_size == 0) {
+    fprintf(stderr, "Internal Compiler Error: Cannot read/write a list with zero-sized buckets!\n");
+    exit(1);
+  }
+
   size_t bucket = idx / list->bucket_size;
   size_t bucket_idx = idx % list->bucket_size;
 
@@ -36,6 +45,11 @@ void* list_get(List* list, size_t idx) {
 }
 
 size_t list_append(List* list, void* value) {
+  if (list->bucket_size == 0) {
+    fprintf(stderr, "Internal Compiler Error: Cannot read/write a list with zero-sized buckets!\n");
+    exit(1);
+  }
+
   if (list->length >= list->capacity) {
     size_t bucket_count = list->capacity / list->bucket_size;
     list->buckets = realloc(list->buckets, (bucket_count + 1) * sizeof(void*));
@@ -52,9 +66,12 @@ size_t list_append(List* list, void* value) {
 }
 
 void free_list(List* list) {
-  size_t bucket_count = list->capacity / list->bucket_size;
+  if (list->bucket_size > 0) {
+    size_t bucket_count = list->capacity / list->bucket_size;
 
-  for (size_t i = 0; i < bucket_count; i++) free(list->buckets[i]);
-  free(list->buckets);
+    for (size_t i = 0; i < bucket_count; i++) free(list->buckets[i]);
+    free(list->buckets);
+  }
+
   free(list);
 }
