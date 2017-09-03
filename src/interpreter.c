8025b8037508fdce69d84e94fa6ad01f526383d0
Pieter van de Bruggen <pvande@gmail.com>
2017-09-02 23:23:02 -0700

    Properly enable out-of-order declarations.

--- src/interpreter.c
+++ src/interpreter.c
@@ -1,32 +1,46 @@
+void interpreter_begin_dependency_initialization(CompilationWorkspace* ws, AstNode* decl) {
+  if (decl->flags & NODE_INITIALIZING) return;
+
+  for (size_t i = 0; i < ws->initializers.length; i++) {
+    AstNode* init = list_get(&ws->initializers, i);
+    if (init->lhs != decl) continue;
+
+    assert(init->bytecode_id != -1);
+
+    decl->flags |= NODE_INITIALIZING;
+
+    VmState* initializer = malloc(sizeof(VmState));
+    initializer->fp = -1;
+    initializer->sp = -1;
+    initializer->ip = 0;
+    initializer->id = init->bytecode_id;
+    initializer->waiting_on = NULL;
+    pipeline_emit_execute_job(ws, initializer);
+  }
+}
+
+void interpreter_finish_dependency_initialization(CompilationWorkspace* ws, size_t bytecode_id) {
+  for (size_t i = 0; i < ws->initializers.length; i++) {
+    AstNode* node = list_get(&ws->initializers, i);
+    if (node->bytecode_id != bytecode_id) continue;
+
+    node->lhs->flags &= ~NODE_INITIALIZING;
+    node->lhs->flags |= NODE_INITIALIZED;
+    break;
+  }
+}
+
 bool perform_execute_job(Job* job) {
   VmState* state = job->vm_state;
-  List* initializers = &job->ws->initializers;
+  CompilationWorkspace* ws = job->ws;
 
-  size_t* bytecode = list_get(&job->ws->bytecode, state->id);
+  size_t* bytecode = list_get(&ws->bytecode, state->id);
 
   if (state->waiting_on) {
     if (state->waiting_on->flags & NODE_INITIALIZED) {
       state->waiting_on = NULL;
     } else {
-      if (!(state->waiting_on->flags & NODE_INITIALIZING)) {
-        for (size_t i = 0; i < initializers->length; i++) {
-          AstNode* init = list_get(initializers, i);
-          if (init->lhs != state->waiting_on) continue;
-
-          assert(init->bytecode_id != -1);
-
-          VmState* init_vm = malloc(sizeof(VmState));
-          init_vm->fp = -1;
-          init_vm->sp = -1;
-          init_vm->ip = 0;
-          init_vm->id = init->bytecode_id;
-          init_vm->waiting_on = NULL;
-          pipeline_emit_execute_job(job->ws, init_vm);
-
-          state->waiting_on->flags |= NODE_INITIALIZING;
-        }
-      }
-
+      interpreter_begin_dependency_initialization(ws, state->waiting_on);
       return 0;
     }
   }
@@ -37,15 +51,7 @@ bool perform_execute_job(Job* job) {
     switch (bytecode[state->ip++]) {
       case BC_EXIT: {
         if (state->fp == -1) {
-          for (size_t i = 0; i < initializers->length; i++) {
-            AstNode* node = list_get(initializers, i);
-            if (node->bytecode_id != state->id) continue;
-
-            node->lhs->flags &= ~NODE_INITIALIZING;
-            node->lhs->flags |= NODE_INITIALIZED;
-            break;
-          }
-
+          interpreter_finish_dependency_initialization(ws, state->id);
           return 1;
         }
 
@@ -95,6 +101,9 @@ bool perform_execute_job(Job* job) {
 
         if (!(decl->flags & NODE_INITIALIZED)) {
           state->ip -= 2;
+          state->waiting_on = decl;
+
+          interpreter_begin_dependency_initialization(ws, state->waiting_on);
           return 0;
         }
 
